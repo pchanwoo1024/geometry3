5dc704c0edf6bd2e6dd0448e2739af0bcc704bab
pchanwoo1024 <pchanwoo1024@gmail.com>
2025-07-27 12:36:11 +0900

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,3 @@
-# database.py
-
 FOOD_DB = {
     "world_cone": {
         "name_kr": "월드콘",
@@ -10,15 +8,26 @@ FOOD_DB = {
         },
         "allergies": ["대두", "밀", "우유", "땅콩"]
     },
-    "lotte_sand": {
-        "name_kr": "롯데샌드 오리지널",
-        "geometry": {"circularity": 0.95, "aspect_ratio": 1.0},
-        "nutrition": {
-            "열량": "265 kcal", "나트륨": "200 mg", "탄수화물": "37 g", "당류": "18 g",
-            "지방": "12 g", "포화지방": "7 g", "단백질": "3 g"
-        },
-        "allergies": ["밀", "대두", "우유"]
-    },
+
+-   "lotte_sand": {
+-       "name_kr": "롯데샌드 오리지널",
+-       "geometry": {"circularity": 0.95, "aspect_ratio": 1.0},
+-       "nutrition": {
+-           "열량": "265 kcal", "나트륨": "200 mg", "탄수화물": "37 g", "당류": "18 g",
+-           "지방": "12 g", "포화지방": "7 g", "단백질": "3 g"
+-       },
+-       "allergies": ["밀", "대두", "우유"]
+-   },
+
++   "bbungtwigi": {
++       "name_kr": "뻥튀기",
++       "geometry": {"circularity": 0.90, "aspect_ratio": 1.00},
++       "nutrition": {
++           # TODO: 실제 뻥튀기 영양 정보로 업데이트해주세요
++       },
++       "allergies": []
++   },
+
     "demi_soda": {
         "name_kr": "데미소다 애플",
         "geometry": {"circularity": 0.7, "aspect_ratio": 0.55},
